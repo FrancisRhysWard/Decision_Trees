975031d2d467eabf06bfec1803b7493a396b5304
az519 <alexey.zakharov19@imperial.ac.uk>
2019-11-03 23:24:37 +0000

    minor changes

--- co553-cbc-dt/assign_unique_id.py
+++
@@ -1,9 +0,0 @@
-
-def assign_id(node_list):
-
-    # Iterate through the node_list
-    for layer_idx, layer in enumerate(node_list):
-        for node_idx, node in enumerate(layer):
-
-            # Assign unique id
-            node.id = int(str(layer_idx) + str(node_idx))
